7e481bc3881affe3dc3480b1de0f9f28daccfeed
Hassan Alwayel <hsn.alwayel@gmail.com>
2020-07-20 17:37:11 +0300

    app.delete Done

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 
-const products = require("./products");
+let products = require("./products");
 
 app.get("/", (req, res) => {
     console.log("Hello");
@@ -14,6 +14,17 @@ app.get("/products", (req, res) => {
     res.json(products);
 });
 
+app.delete("/products/:productId", (req, res) => {
+    const { productId } = req.params;
+    const foundProduct = products.find((product) => product.id === +productId)
+    if (foundProduct) {
+        products = products.filter((_product) => _product.id !== +productId);
+        res.status(204).end();
+    } else {
+        res.status(404);
+        res.json({ message: "Product not found!" });
+    }
+});
 app.listen(8000, () => {
     console.log("The application is running on localhost:8000");
 });
