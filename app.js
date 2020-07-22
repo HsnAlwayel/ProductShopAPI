5a96cf887908db033aee64b8b0fd18bb0a3b1a6b
Hassan Alwayel <hsn.alwayel@gmail.com>
2020-07-22 20:12:00 +0300

    DataBase Created

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
+const db = require("./db/db");
 //Routes
 const productRoutes = require("./routes/products");
 
 //Express instance
 const app = express();
+
+// const run = async () => {
+//     try {
+//         await db.authenticate();
+//         console.log("Connection to the database successful!");
+//     } catch (error) {
+//         console.error("Error connecting to the database: ", error);
+//     }
+
+//     await app.listen(8000, () => {
+//         console.log("The application is running on localhost:8000");
+//     });
+// };
+
+// run();
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -14,6 +29,6 @@ app.use(bodyParser.json());
 app.use("/products", productRoutes);
 
 //localhost:8000
-app.listen(8000, () => {
+app.listen(8001, () => {
     console.log("The application is running on localhost:8000");
 });
